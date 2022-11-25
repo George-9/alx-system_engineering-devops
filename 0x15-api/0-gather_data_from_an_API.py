63ba835da1820b31852633cdaf987000d8253c66
George-9 <georgemuigainjau@gmail.com>
2022-11-25 13:16:04 +0300

    a Python script that, using this https://jsonplaceholder.typicode.com/, for a given employee ID, returns information about his/her TODO list progress

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,70 +4,36 @@
     shows the completed tasks
 """
 
-
 import json
 from sys import argv
-from urllib import urllib.request as request
+from urllib import request as request
 
 user_request = request.urlopen('https://jsonplaceholder.typicode.com/users')
 todos_request = request.urlopen('https://jsonplaceholder.typicode.com/todos')
 
-
-def getEmployeeName(employees=[], id=0):
-    """gets the given employer name by id
-
-       Args:
-        employees: The list of employees
-        id: The employee's id
-
-       Returns:
-         employee's name(str)
-
-       Raises:
-         no error
-    """
-    employeeName = ''
-    for employee in employees:
-        if employee.get('id') == id:
-            employeeName = employee.get('name')
-    return employeeName
-
-
-def getCompletedTodos(todos=[], id=0):
-    """gets the tasks' info of the employee
-       Args:
-        todos: list of employees
-        id: The employee's id
-
-       Returns:
-         number of taks(int),
-         number of completed tasks(int),
-         list of the completed tasks
-
-       Raises:
-         no error
-    """
-    count = 0
-    done = 0
-    done_list = []
-    for todo in todos:
-        if (todo.get('userId') == id):
-            if (todo.get('completed') is True):
-                done_list.append(todo.get('title'))
-                done += 1
-            count += 1
-    return count, done, done_list
-
-
-employees = json.loads(user_request.read())
-todos = json.loads(todos_request.read())
-
 employeeId = int(argv[1])
+employees = json.loads(user_request.read())
 
 """employee name"""
-e = getEmployeeName(employees, employeeId)
-totalTasks, totalDone, completedList = getCompletedTodos(todos, employeeId)
+e = ''
+for employee in employees:
+    if employee.get('id') == id:
+        e = employee.get('name')
 
-print(f'Employee {e} is done with tasks ({totalDone}/{totalTasks}):')
-for t in completedList:
-    print(f'\t{t}')
+count = 0
+done = 0
+done_list = []
+
+todos = json.loads(todos_request.read())
+
+for todo in todos:
+    if (employeeId == todo.get('userId')):
+        if (todo.get('completed') is True):
+            done_list.append(todo.get('title'))
+            done += 1
+        count += 1
+
+if __name__ == '__main__':
+    print(f'Employee {e} is done with tasks ({done}/{count}):')
+    for t in done_list:
+        print(f'\t{t}')
